c9f6b57f0e5a76ff09b240835bbb80428fc318df
Nakhwa Azizah <93728502+nakhwaazizah@users.noreply.github.com>
2024-08-12 14:07:18 +0700

    Update load_model.py

--- load_model.py
+++ load_model.py
@@ -2,16 +2,19 @@ from transformers import BertTokenizer, BertForSequenceClassification
 
 # Dictionary to map model names to their paths
 model_paths = {
-    "cahya/bert-base-indonesian-522M": r"DasboardBert/cahya/model.safetensors",
-    "indobenchmark/indobert-base-p2": r"DasboardBert/indobench/model.safetensors",
-    "indolem/indobert-base-uncased": r"DasboardBert/indolem/model.safetensors",
-    "mdhugol/indonesia-bert-sentiment-classification": r"DasboardBert/mdhugol/model.safetensors"
+    "cahya/bert-base-indonesian-522M": "modelcahya",
+    "indobenchmark/indobert-base-p2": "modelindobench",
+    "indolem/indobert-base-uncased": "modelindolem",
+    "mdhugol/indonesia-bert-sentiment-classification": "modelmdhugol"
 }
 
 # Function to load the selected model
 def load_model(model_name):
-    path = model_paths[model_name]
-    tokenizer = BertTokenizer.from_pretrained(path)
-    model = BertForSequenceClassification.from_pretrained(path)
+    base_path = model_paths[model_name]
+    weights_path = f"{base_path}/model.safetensors"
+    
+    # Load tokenizer and model using correct paths
+    tokenizer = BertTokenizer.from_pretrained(base_path)
+    model = BertForSequenceClassification.from_pretrained(base_path, weights_name=weights_path)
     model.eval()
     return tokenizer, model
