fda4cd9e43b4fe8e95e37a81baecf4d5533b5431
Nakhwa Azizah <93728502+nakhwaazizah@users.noreply.github.com>
2024-08-27 08:56:00 +0700

    Update deteksi_upload.py

--- deteksi_upload.py
+++ deteksi_upload.py
@@ -4,6 +4,48 @@ from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
 from test import predict_hoax, evaluate_model_performance
 from load_model import load_model
 from styles import COMMON_CSS
+from google.cloud import storage
+from io import StringIO
+import os
+from datetime import datetime
+import pytz
+
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\Lenovo\Downloads\DasboardBert\inbound-source-431806-g7-e49e388ce0be.json"
+
+def download_json_from_gcs(bucket_name, source_blob_name, destination_file_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+    print(f"Downloaded storage object {source_blob_name} from bucket {bucket_name} to local file {destination_file_name}.")
+
+# Konfigurasi
+bucket_name = 'dashboardhoax-bucket'
+source_blob_name = 'dashboardhoax-bucket/inbound-source-431806-g7-e49e388ce0be.json'
+destination_file_name = '/tmp/json-file.json'
+
+# Unduh file JSON dari GCS
+download_json_from_gcs(bucket_name, source_blob_name, destination_file_name)
+
+# Gunakan file JSON yang diunduh
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = destination_file_name
+
+def save_corrections_to_gcs(bucket_name, file_name, correction_data):
+    client = storage.Client()
+    bucket = client.bucket("dashboardhoax-bucket")
+    blob = bucket.blob("koreksi_pengguna_file.csv")
+
+    if blob.exists():
+        existing_data = blob.download_as_string().decode('utf-8')
+        existing_df = pd.read_csv(StringIO(existing_data))
+    else:
+        existing_df = pd.DataFrame(columns=['Timestamp', 'Label_id', 'Label', 'Title', 'Content', 'Fact', 'References', 'Classification', 'Datasource', 'Result_Detection', 'Result_Correction'])
+
+    new_data_df = pd.DataFrame(correction_data)
+    updated_df = pd.concat([existing_df, new_data_df], ignore_index=True)
+
+    updated_csv_data = updated_df.to_csv(index=False)
+    blob.upload_from_string(updated_csv_data, content_type='text/csv')
 
 def load_data(file):
     return pd.read_csv(file)
@@ -39,6 +81,7 @@ def show_deteksi_upload():
 
         grid_options = GridOptionsBuilder.from_dataframe(df)
         grid_options.configure_pagination(paginationAutoPageSize=False, paginationPageSize=10)
+        grid_options.configure_default_column(cellStyle={'fontSize': '12px'})
         gridOptions = grid_options.build()
 
         AgGrid(
@@ -57,17 +100,16 @@ def show_deteksi_upload():
                 st.error(f"Terjadi kesalahan saat deteksi: {e}")
 
     if st.session_state.df is not None:
-
-        accuracy, precision, recall, f1 = evaluate_model_performance(st.session_state.df, tokenizer, model)
-        performance_text = (
-            f"**Performansi Model**\n\n"
-            f"**Accuracy:** {round(accuracy, 2)}&nbsp;&nbsp;"
-            f"**Precision:** {round(precision, 2)}&nbsp;&nbsp;"
-            f"**Recall:** {round(recall, 2)}&nbsp;&nbsp;"
-            f"**F1 Score:** {round(f1, 2)}"
-        )
-
-        st.success(performance_text)
+        if 'Label' in st.session_state.df.columns:
+            accuracy, precision, recall, f1 = evaluate_model_performance(st.session_state.df, tokenizer, model)
+            performance_text = (
+                f"*Performansi Model*\n\n"
+                f"*Accuracy:* {round(accuracy, 2)}&nbsp;&nbsp;"
+                f"*Precision:* {round(precision, 2)}&nbsp;&nbsp;"
+                f"*Recall:* {round(recall, 2)}&nbsp;&nbsp;"
+                f"*F1 Score:* {round(f1, 2)}"
+            )
+            st.success(performance_text)
 
         st.markdown("<h6 style='font-size: 16px; margin-bottom: 0;'>Hasil Deteksi</h6>", unsafe_allow_html=True)
 
@@ -77,6 +119,7 @@ def show_deteksi_upload():
         grid_options = GridOptionsBuilder.from_dataframe(df_reordered)
         grid_options.configure_pagination(paginationAutoPageSize=False, paginationPageSize=10)
         grid_options.configure_default_column(editable=True, groupable=True)
+        grid_options.configure_default_column(cellStyle={'fontSize': '12px'})
         gridOptions = grid_options.build()
 
         grid_response = AgGrid(
@@ -99,25 +142,35 @@ def show_deteksi_upload():
             st.session_state.df = edited_df.copy()
 
             if not corrected_df.empty:
-                corrected_df['Result_Correction'] = corrected_df.apply(lambda row: 
-                    'HOAX' if (row['Result_Detection'] == 'NON-HOAX' and row['Correction']) else 
-                    ('NON-HOAX' if (row['Result_Detection'] == 'HOAX' and row['Correction']) else row['Result_Detection']), 
-                    axis=1
-                )
+                expected_cols = ['Timestamp', 'Result_Detection', 'Result_Correction', 'Label_id', 'Label', 'Title', 'Content', 'Fact', 'References', 'Classification', 'Datasource']
+                existing_cols = [col for col in expected_cols if col in corrected_df.columns]
 
-                cols = ['Result_Correction', 'Result_Detection'] + [col for col in corrected_df.columns if col not in ['Result_Detection', 'Result_Correction', 'Label_id']]
-                corrected_df = corrected_df[cols]
+                # Tambahkan Timestamp hanya untuk penyimpanan
+                wib = pytz.timezone('Asia/Jakarta')
+                corrected_df['Timestamp'] = datetime.now(wib).strftime('%Y-%m-%d %H:%M:%S')
+
+                corrected_df_to_display = corrected_df[existing_cols]
 
                 st.markdown("<h6 style='font-size: 16px; margin-bottom: 0;'>Data yang Dikoreksi</h6>", unsafe_allow_html=True)
-                st.dataframe(corrected_df.drop(columns=['Correction']), use_container_width=True, hide_index=True)
+                st.dataframe(corrected_df_to_display, use_container_width=True, hide_index=True)
             else:
                 st.write("Tidak ada data yang dikoreksi.")
         
         if st.button("Simpan", key="corrected_data"):
             if 'df' in st.session_state:
-                corrected_df = st.session_state.df[st.session_state.df['Correction']].drop(columns=['Correction'])
+                corrected_df = st.session_state.df[st.session_state.df['Correction']].copy()
+                wib = pytz.timezone('Asia/Jakarta')
+                corrected_df['Timestamp'] = datetime.now(wib).strftime('%Y-%m-%d %H:%M:%S')
+                corrected_df = corrected_df.drop(columns=['Correction'])
+
                 if not corrected_df.empty:
-                    corrected_df.to_csv("corrected_df.csv", index=False)
+                    bucket_name = "your-bucket-name"
+                    file_name = "corrected_upload_data.csv"
+                    
+                    correction_data = corrected_df.to_dict(orient='records')
+                    
+                    save_corrections_to_gcs(bucket_name, file_name, correction_data)
+                    
                     st.success("Data telah disimpan.")
                     st.session_state.corrected_df = corrected_df
                 else:
